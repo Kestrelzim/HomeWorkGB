7c0401608749ed084c532ef01e8607a544c8c98d
Kestrel <zimaleksey@gmail.com>
2022-04-03 09:38:04 +0500

    Перенос перво домашки

--- hw/main.swift
+++ hw/main.swift
@@ -9,5 +9,35 @@ import Foundation
 
 print("Hello, World!")
 
-FDSAJDLKJKL
-ICMPV6CTL_ND6_OPTIMISTIC_DAD
+///Квадратное уравнение не решил
+let a = 1
+let b = 4
+let c = 2
+
+
+///Даны катеты прямоугольного треугольника. Найти площадь, периметр и гипотенузу треугольника
+let CatetA : Float = 13.00 // катет 1
+let CatetB : Float = 27.00 // катет 2
+let Chyp = sqrtf(Float(Int(powf(Float(CatetA), 2)+powf(Float(CatetB), 2)))) // гипотенуза ?
+let STriangle = (CatetA * CatetB)/2 // площадь по катетам
+let PeremetTriangle = CatetA + CatetB + Chyp // периметр по трём
+
+print("Площадь треугольника")  // не понял как вписать в принт текст и вывод, сделал в итоге громоздко
+print(STriangle);
+print("Гепотенуза");
+print(Chyp);
+print("Периметр треугольника");
+print(PeremetTriangle);
+
+/// Проценты по вклад
+
+var value = 100 //вклад
+let procent = 25 //процент
+let year = 5 //срок
+let procentPlus = (value * procent) / 100 // cумма за один год
+
+for value in 1...year { //тут попытка как то прогнать сколько будет процентов за 5 лет и сложить с первоначальным вкладом
+print("\(value)  \(procentPlus * year)")
+}
+print(value)
+
